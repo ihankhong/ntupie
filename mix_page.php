9c78f62de1a9fd22d6789397afc320e4d3b1d156
ihankhong <hank790315@gmail.com>
2017-11-29 05:06:47 +0800

    mix_page finish

---
+++ mix_page.php
@@ -0,0 +1,103 @@
+<?php
+ require_once("header.php");
+ require_once("carousel.php");
+?>
+<section class="content-wrap">
+	<div class="container-full clearfix">
+		<div class="col-xs-12 col-sm-4 col-md-4 col-lg-3">
+			<div class="sidebar-wrap">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sideNavbar" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<h3>重要公告</h3>
+					<img src="images/page/sidebar/text-page.png" class="img-responsive hidden-xs" />
+				</div>
+				<div id="sideNavbar" class="list navbar-collapse collapse" aria-expanded="false">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="#">資訊講座</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">重要公告 <span class="arrow"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">榮譽捷報</a></li>
+								<li><a href="#">演講資訊</a></li>
+							</ul>
+						</li>
+						<li><a href="#">演講資訊</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">IEET認證 <span class="arrow"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">IEET認證</a></li>
+							</ul>
+						</li>
+						<li><a href="#">校園活動</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">學生專區 <span class="arrow"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">獎助學金</a></li>
+							</ul>
+						</li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">招生資訊 <span class="arrow"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">大學部</a></li>
+							</ul>
+						</li>
+						<li><a href="#">系友專區</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-8 col-md-8 col-lg-9">
+			<div class="content-box">
+				<h1>通過IEET認證</h1>
+				<h3>IEET認證在世界一流大學多參與認證<div class="border"></div></h3>
+				<div class="content">
+					<p>中華工程教育學會(IEET)成立於2003年，為一非官方、非營利的社團法人。IEET是國內首家受教育部認可的專業評鑑機構，主要業務為規劃及執行符合國際標準的工程教育(EAC)、資訊教育(CAC)、技術教育(TAC)、建築教育(AAC)認證及設計教育(DAC)認證。國內已有84所大學校院的546個系所參與IEET認證。</p>
+					<div class="mix clearfix">
+						<h3>中文版</h3>
+						<div class="row">
+							<div class="col-xs-12 col-sm-4">
+								<img src="images/page/proof/mix_page-1.png" class="img-responsive center-block" />
+							</div>
+							<div class="col-xs-12 col-sm-8">
+								<div class="item">
+									<label class="sub-title">透過認證機制</label>
+									<p>IEET訂定及維持國際間認可的專業核心能力及倫理規範</p>
+								</div>
+								<div class="item">
+									<label class="sub-title">藉此維繫業界</label>
+									<p>政府及整體社會對於我國未來工程、資訊、技術及建築領域專業人才的信心。</p>
+								</div>
+								<div class="link"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="http://www.ieet.org.tw/">http://www.ieet.org.tw/</a></div>
+							</div>
+						</div>
+						<h3>英文版</h3>
+						<div class="row">
+							<div class="col-xs-12 col-sm-4">
+								<img src="images/page/proof/mix_page-2.png" class="img-responsive center-block" />
+							</div>
+							<div class="col-xs-12 col-sm-8">
+								<div class="item">
+									<label class="sub-title">Through the certification mechanism</label>
+									<p>IEET establishes and maintains internationally recognized professional core competencies and ethical practices.</p>
+								</div>
+								<div class="item">
+									<label class="sub-title">To maintain the industry</label>
+									<p>The Government and the community as a whole in the future of engineering, information, technology and construction professionals in the field of confidence.</p>
+								</div>
+								<div class="link"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="http://www.ieet.org.tw/">http://www.ieet.org.tw/</a></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<?php
+ require_once("footer.php");
+?>
