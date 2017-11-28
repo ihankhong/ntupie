7fc29b604c78506ef4e37a849a52744828b4747e
ihankhong <hank790315@gmail.com>
2017-11-28 22:31:58 +0800

    list_page 完成

---
+++ list_page.php
@@ -0,0 +1,76 @@
+<?php
+ require_once("header.php");
+ require_once("carousel.php");
+?>
+<section class="content-wrap">
+	<div class="container-full clearfix">
+		<div class="col-xs-12 col-sm-4 col-md-4 col-lg-3">
+			<div class="sidebar-wrap">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sideNavbar" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<h3>重要公告</h3>
+					<img src="images/page/sidebar/text-page.png" class="img-responsive hidden-xs" />
+				</div>
+				<div id="sideNavbar" class="list navbar-collapse collapse" aria-expanded="false">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="#">資訊講座</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">重要公告 <span class="arrow"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">榮譽捷報</a></li>
+								<li><a href="#">演講資訊</a></li>
+							</ul>
+						</li>
+						<li><a href="#">演講資訊</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">IEET認證 <span class="arrow"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">IEET認證</a></li>
+							</ul>
+						</li>
+						<li><a href="#">校園活動</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">學生專區 <span class="arrow"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">獎助學金</a></li>
+							</ul>
+						</li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">招生資訊 <span class="arrow"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">大學部</a></li>
+							</ul>
+						</li>
+						<li><a href="#">系友專區</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-8 col-md-8 col-lg-9">
+			<div class="content-box">
+				<h1>演講資訊</h1>
+				<h3>臺北大學資工系學生演講資訊列表整理<div class="border"></div></h3>
+				<div class="content">
+					<div class="list"><span>置頂公告</span><a href="#">資訊工程學系碩士班(含學士班)104學年度第2學期演講時程</a></div>
+					<div class="list"><span>置頂公告</span><a href="#">資訊工程學系104學年度第2學期工程倫理演講時程</a></div>
+					<div class="list"><span>2017.09.30</span><a href="#">專題研討(106/9/29) －林伯昰教授 (國立交通大學影像生醫所教授)</a></div>
+					<div class="list"><span>2017.09.29</span><a href="#">專題研討(106/10/6) －蔣榮先教授 (成功大學資訊工程學系教授)</a></div>
+					<div class="list"><span>2017.09.28</span><a href="#">專題研討(106/9/22) －劉傳銘 教授 (台北科技大學資工系教授)</a></div>
+					<div class="list"><span>2017.09.27</span><a href="#">專題研討(106/9/15) －李潤容 博士 (工研院技術副組長/清華大學兼任副教授)</a></div>
+					<div class="list"><span>2017.09.26</span><a href="#">暑期演講 (2017/8/24) - 廖文華 教授 (大同大學 資訊經營系教授兼校務研究辦公室執行長)</a></div>
+					<div class="list"><span>2017.09.25</span><a href="#">專題研討(106/6/9) －曾柏軒 國立台北科技大學電子工程系</a></div>
+					<div class="list"><span>2017.09.24</span><a href="#">專題研討(2017/5/26) －孔令傑 教授(國立台灣大學資訊管理學系)</a></div>
+					<div class="list"><span>2017.09.23</span><a href="#">專題研討(2017/5/19) －曾煜棋 院長(交大資訊工程系講座教授兼資訊學院院長)(演講時間為14:00開始)</a></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<?php
+ require_once("footer.php");
+?>
