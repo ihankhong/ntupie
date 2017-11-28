d8a7e0a86fb5746ff9cb0b4b03d3ea3614b53f3e
ihankhong <hank790315@gmail.com>
2017-11-28 22:08:16 +0800

    text_page 完成

--- text_page.php
+++ text_page.php
@@ -13,11 +13,12 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<h3>最新消息</h3>
+					<h3>重要公告</h3>
+					<img src="images/page/sidebar/text-page.png" class="img-responsive hidden-xs" />
 				</div>
 				<div id="sideNavbar" class="list navbar-collapse collapse" aria-expanded="false">
 					<ul class="nav navbar-nav">
-						<li class="active"><a href="index.php">首頁</a></li>
+						<li class="active"><a href="#">資訊講座</a></li>
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">重要公告 <span class="arrow"></span></a>
 							<ul class="dropdown-menu">
@@ -25,13 +26,14 @@
 								<li><a href="#">演講資訊</a></li>
 							</ul>
 						</li>
+						<li><a href="#">演講資訊</a></li>
 						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">系所簡介 <span class="arrow"></span></a>
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">IEET認證 <span class="arrow"></span></a>
 							<ul class="dropdown-menu">
-								<li><a href="#">系史館</a></li>
+								<li><a href="#">IEET認證</a></li>
 							</ul>
 						</li>
-						<li><a href="#">師資介紹</a></li>
+						<li><a href="#">校園活動</a></li>
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">學生專區 <span class="arrow"></span></a>
 							<ul class="dropdown-menu">
@@ -49,7 +51,21 @@
 				</div>
 			</div>
 		</div>
-		<div class="col-xs-12 col-sm-8 col-md-8 col-lg-9"></div>
+		<div class="col-xs-12 col-sm-8 col-md-8 col-lg-9">
+			<div class="content-box">
+				<h1>系所簡介</h1>
+				<h3>臺北大學資工系學生2018-19學年出國交換至香港理工大學公告<div class="border"></div></h3>
+				<div class="content">
+					<p>國立台北大學的前身是國立中興大學法商學院，已有悠久的法商與人文基礎，現有台北及三峽二大校區。台北校區位於民生東路及建國北路交叉口，為台北市的商業特區；三峽校區位於南港軟體園區、中研院及新竹科學園區間，三峽交流道旁，為台灣高科技產業的中心點，交通便利。校區幅員遼闊、交通便利。三峽校區依山傍水、風景秀麗，為一所孕育高級人才的美麗大學。為使科技與人文均衡發展，達到全人教育的教育理念，奉准於九十三學年度成立資訊工程學系，招收大學部之優秀人才一班。並奉准於九十七學年度成立碩士班。</p>
+					<p>本系在法商與人文為主的大學成立第一所高科技學系，除了有豐富的專業課程</p>
+					<p>訓練外，尚有以理工為主的學校所較欠缺的法商與人文素養的薰陶。優秀的您在此環境下將培養成為一位文武兼備的高級人才。本系的專業訓練將注重資訊理論與實務的結合，並透過各種學術活動及與產業交流，使同學得以接收最新科技資訊，培養同學成為理論與實務兼備、人文與科技整合的高級人才。</p>
+					<p>本系將不定期舉辦各種理論與實務的專題演講，透過這類活動可讓師生了解現有產業與學術的趨勢，掌握趨勢便可以掌握未來。</p>
+					<p>本系已設立電子電路實驗室、數位及微算機實驗室、網路實驗室及多媒體與影像、行動計算、通訊網路、分散式系統、系統晶片等多個研究室，本系為全新的系，所有設備都是全新的設備。</p>
+					<p>目前的電子、資訊及通訊產業是最有發展的高科技產業之一，畢業同學可以選擇半導體產業、IC設計產業、資訊軟硬體產業、通訊產業等，跨足這三大產業，同學的出路是無限的寬廣，未來的科技新貴就是您。且本校位於新竹科學園區與大台北工業區間(包括中壢工業區、土城到新店工業區之間)，產業資源最為豐富，畢業同學一定可以很快找到高科技產業，成為明日之星。</p>
+					<p>國立台北大學在電機資訊方面目前共有四個單位，分別是資訊工程系所(即本系)、通訊工程研究所、電機工程研究所、以及資訊管理研究所。同時於九十六學年度成立電機資訊學院。未來此一學院將朝向兩系(電機系與資工系)及多研究所的方向發展。在資訊工程領域，已成立以網路與多媒體為主要研究特色之資訊工程研究所，中長期計畫將成立電腦工程、晶片設計、數位內容、虛擬實境、認知工程、電子運籌等研究所。資訊工程學系本身是以大學部之教學為目標，所有教師均是與前述之獨立研究所所合聘，並指導論文。除了前述之研究所外，電機資訊學院將與國立臺北大學商學院合作成立財務工程研究中心，並與法律學院成立智慧財產權研究中心。</p>
+				</div>
+			</div>
+		</div>
 	</div>
 </section>
 <?php
